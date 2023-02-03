72728d72dd9fb1a4aeb4a13a1f75787c57522539
Artsiom <artemorsha@mail.ru>
2023-02-04 02:34:43 +0300

    add lineThrough

--- script/script.js
+++ script/script.js
@@ -6,15 +6,19 @@ window.addEventListener('DOMContentLoaded', () => {
 		input = document.querySelector('#input'),
 		addTaskBtn = document.querySelector('.btn-to-do');
 
-
 	let taskArr = [];
-
 
 	if (localStorage.getItem('todo')) {
 		taskArr = JSON.parse(localStorage.getItem('todo'));
 		displayTasks();
 	}
 
+	input.addEventListener('keyup', (e) => {
+		if (input.value.trim() == 0) {
+			alert('Enter task!');
+			input.value = '';
+		}
+	});
 
 	addTaskBtn.addEventListener('click', () => {
 		if (input.value.length === 0) {
@@ -23,35 +27,31 @@ window.addEventListener('DOMContentLoaded', () => {
 		const taskObj = {
 			message: input.value,
 			crossed: false,
-			important: false,
+			text: false,
+			btn: false,
 		};
 		taskArr.push(taskObj);
-
 
 		displayTasks();
 		input.value = '';
 		localStorage.setItem('todo', JSON.stringify(taskArr));
 	});
 
-
-
 	function displayTasks() {
 
 		let li = '';
 
 		taskArr.forEach((item, i) => {
-
 			li += `
 			<li>
 				<input type="checkbox" id="input-${i}" ${item.crossed ? 'checked' : ''}>
-				<p class="text-value input-${i}">${item.message}</p>
-				<button class="btn btn-delete">Delete</button>
+				<p class="${item.text ? 'active ' : ''}text-value input-${i} btn-${i}">${item.message}</p>
+				<button class="btn btn-${i} btn-delete ${item.btn ? 'delete ' : ''}" id="btn-${i}">Delete</button>
 			</li>
 		`;
 		});
 
 		wrapperTask.innerHTML = li;
-
 	}
 
 	wrapperTask.addEventListener('change', (e) => {
@@ -59,19 +59,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		let forText = wrapperTask.querySelector('.' + idInput);
 		let textValue = forText.innerHTML;
 
-		taskArr.forEach(item => {
+		taskArr.forEach((item, i) => {
 			if (item.message === textValue) {
 				item.crossed = !item.crossed;
+				item.text = !item.text;
 				localStorage.setItem('todo', JSON.stringify(taskArr));
+				displayTasks();
 			}
+
 		});
 	});
-
-	// wrapperTask.addEventListener('click', (e) => {
-	// 	taskArr.forEach(item => {
-	// 		if (item.)
-	// 	});
-	// });
 
 
 
